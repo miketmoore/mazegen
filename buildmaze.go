fda7a9ebf47abf107f81f235754aaadc8f02edb6
Michael Moore <mike@miketmoore.com>
2021-05-09 21:54:44 -0400

    fix

--- buildmaze.go
+++ buildmaze.go
@@ -5,6 +5,32 @@ import (
 	"math/rand"
 )
 
+type History struct {
+	coordinates []*Coordinates
+}
+
+func NewHistory() *History {
+	return &History{
+		coordinates: []*Coordinates{},
+	}
+}
+
+func (history *History) Length() int {
+	return len(history.coordinates)
+}
+
+func (history *History) GetLast() *Coordinates {
+	return history.coordinates[history.Length()-1]
+}
+
+func (history *History) Push(coordinates *Coordinates) {
+	history.coordinates = append(history.coordinates, coordinates)
+}
+
+func (history *History) DeleteLast() {
+	history.coordinates = history.coordinates[:history.Length()-1]
+}
+
 func BuildMaze(rows, columns int, random *rand.Rand) (*Grid, error) {
 	grid, err := NewGrid(rows, columns, random)
 	if err != nil {
@@ -12,12 +38,15 @@ func BuildMaze(rows, columns int, random *rand.Rand) (*Grid, error) {
 		return nil, fmt.Errorf("error building new grid")
 	}
 
-	history := []*Coordinates{grid.RandomCoordinates()}
+	// history := []*Coordinates{grid.RandomCoordinates()}
+	history := NewHistory()
+	history.Push(grid.RandomCoordinates())
 
 	running := true
 
 	for running {
-		coordinates := history[len(history)-1]
+		// coordinates := history[len(history)-1]
+		coordinates := history.GetLast()
 		cell := grid.Cell(coordinates)
 		if cell == nil {
 			return nil, fmt.Errorf("cell not found")
@@ -26,8 +55,9 @@ func BuildMaze(rows, columns int, random *rand.Rand) (*Grid, error) {
 		availableWalls := grid.AvailableCellWalls(cell, coordinates)
 
 		if len(availableWalls) == 0 {
-			if len(history) >= 2 {
+			if history.Length() >= 2 {
 				// TODO pop history
+				history.DeleteLast()
 			} else {
 				running = false
 			}
@@ -42,7 +72,8 @@ func BuildMaze(rows, columns int, random *rand.Rand) (*Grid, error) {
 				if adjacentCell != nil && !adjacentCell.Visited {
 					oppositeDirection := adjacentCell.OppositeDirection(availableWall)
 					grid.CarveCellWall(adjacentCoordinates, oppositeDirection)
-					history = append(history, adjacentCoordinates)
+					// history = append(history, adjacentCoordinates)
+					history.Push(adjacentCoordinates)
 				}
 			}
 		}
